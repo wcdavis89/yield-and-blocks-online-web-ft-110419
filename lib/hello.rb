c691e45c9fc850e9af90184fe18369c1f1ab943d
Whittney Davis <wcdavis89@yahoo.com>
2019-11-12 23:46:40 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,8 @@
 def hello_t(array)
-  array.each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  else
-    puts name.upcase
-  end
-  end
+  i=0
+  while i<array.length 
+  i=i+1 
+end
 end
 
 # call your method here!
